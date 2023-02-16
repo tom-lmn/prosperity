f03f878365e57dafd06f3f0f7a7f792dc19a4410
Tom Lehmann <86118912+tom-lmn@users.noreply.github.com>
2023-02-16 19:37:32 +0100

    Update first_algo.py
    added bananas

--- first_algo.py
+++ first_algo.py
@@ -8,6 +8,8 @@ class Trader:
     iteration_count = 0 #das ist 0
     pearl_trades = pd.DataFrame(columns = ['quantity','price'])
     pearl_price_avg = int
+    banana_trades = pd.DataFrame(columns = ['quantity','price'])
+    banana_price_avg = int
 
     def run(self, state: TradingState) -> Dict[str, List[Order]]:
         """
@@ -19,8 +21,11 @@ class Trader:
         
         if hasattr(state, 'market_trades'):
             list_pearl_trades = state.market_trades['PEARLS']
+            list_banana_trades = state.market_trades['BANANAS']
         else:
-            list_pearl_trades = []
+            list_banana_trades = []
+            list_banan_trades = []
+
         
 
 
@@ -29,16 +34,77 @@ class Trader:
 
             # Check if the current product is the 'PEARLS' product, only then run the order logic
             if product == 'PEARLS':
+                
+                # Initialize the list of Orders to be sent as an empty list
                 orders: list[Order] = []				
 				
                 acceptable_price = 10000
                 
                 for trade in list_pearl_trades:
-                    self.pearl_trades.append({'quantity': trade.quantity, 'price': trade.price}, ignore_index=True)	 		 
+                    self.pearl_trades.concat(pd.Series([trade.quantity, trade.price])) 		 
                     
                 
                 if not self.pearl_trades.empty:
                     pearl_price_avg = np.average(a = self.pearl_trades['price'], weights = self.pearl_trades['quantity'])
+                    # Define a fair value for the PEARLS.
+                    acceptable_price = pearl_price_avg
+                   
+                    
+                
+                # Retrieve the Order Depth containing all the market BUY and SELL orders for PEARLS
+                order_depth: OrderDepth = state.order_depths[product]
+
+                
+                # If statement checks if there are any SELL orders in the PEARLS market
+                if len(order_depth.sell_orders) > 0:
+
+                    # Sort all the available sell orders by their price,
+                    # and select only the sell order with the lowest price
+                    best_ask = min(order_depth.sell_orders.keys())
+                    best_ask_volume = order_depth.sell_orders[best_ask]
+
+                    # Check if the lowest ask (sell order) is lower than the above defined fair value
+                    if best_ask < acceptable_price:
+
+                        # In case the lowest ask is lower than our fair value,
+                        # This presents an opportunity for us to buy cheaply
+                        # The code below therefore sends a BUY order at the price level of the ask,
+                        # with the same quantity
+                        # We expect this order to trade with the sell order
+                        print("BUY", str(-best_ask_volume) + "x", best_ask)
+                        orders.append(Order(product, best_ask, -best_ask_volume))
+
+                # The below code block is similar to the one above,
+                # the difference is that it find the highest bid (buy order)
+                # If the price of the order is higher than the fair value
+                # This is an opportunity to sell at a premium
+                if len(order_depth.buy_orders) != 0:
+                    best_bid = max(order_depth.buy_orders.keys())
+                    best_bid_volume = order_depth.buy_orders[best_bid]
+                    if best_bid > acceptable_price:
+                        print("SELL", str(best_bid_volume) + "x", best_bid)
+                        orders.append(Order(product, best_bid, -best_bid_volume))
+
+                # Add all the above the orders to the result dict
+                result[product] = orders
+
+                # Return the dict of orders
+                # These possibly contain buy or sell orders for PEARLS
+                # Depending on the logic above
+
+            if product == 'BANANAS':
+                order_depth: OrderDepth = state.order_depths[product]
+
+                orders: list[Order] = []				
+				
+                acceptable_price = 10000
+                
+                for trade in list_banana_trades:
+                    self.banana_trades.concat({'quantity': trade.quantity, 'price': trade.price}, ignore_index=True)	 		 
+                    
+                
+                if not self.banana_trades.empty:
+                    pearl_price_avg = np.average(a = self.banana_trades['price'], weights = self.banana_trades['quantity'])
                     # Define a fair value for the PEARLS.
                     acceptable_price = pearl_price_avg
                    
@@ -89,16 +155,6 @@ class Trader:
                 # Return the dict of orders
                 # These possibly contain buy or sell orders for PEARLS
                 # Depending on the logic above
-
-            if product == 'BANANAS':
-                order_depth: OrderDepth = state.order_depths[product]
-
-                if len(order_depth.sell_orders) > 0:
-                    best_ask = min(order_depth.sell_orders.keys())
-
-                    #bananamin = best_ask
-
-                    #print("best ask is" + str(bananamin))
 
         self.iteration_count += 1
         print(str(self.iteration_count))

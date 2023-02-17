538105c12dc1b24ebd1e5e213add861dbcad8e9d
Tom Lehmann <86118912+tom-lmn@users.noreply.github.com>
2023-02-17 22:28:54 +0100

    Update second_algo.py

--- second_algo.py
+++ second_algo.py
@@ -5,13 +5,17 @@ import numpy as np
 
 class Trader:
 
+    '''
+    EVERYTHING UP TO iteration_count 
+    HAS TO BE SET MANUALLY
+    '''
     #list of products
     products = ['PEARLS', 'BANANAS']
 
     #dict holding the strategy for each product
     strategy = {
         'PEARLS': 'all_time_avg',
-        'BANANAS': 'floating_avg_fifty'
+        'BANANAS': 'floating_avg'
     }
 
     #guessed beginning prices can be set here
@@ -20,23 +24,22 @@ class Trader:
         'BANANAS': 4900
     }
 
-    
+    #set window size for floating_avg for according products
+    floating_avg_window = {
+        'BANANAS': 50
+    }
 
-    historical_market_trades = Dict[str, List[Trade]]
-
-    number_of_trades = 50
     iteration_count = 0 
-    pearl_trades = pd.DataFrame(columns = ['quantity','price'])
-    pearl_price_avg = int
-    banana_array_fifty = np.ones(number_of_trades)*4900
+
+    #Liste wo für jedes Produkt alle market trades gespeichert werden können, siehe __init__
+    historical_market_trades = {}
+    daily_price = {}
 
     def __init__(self) -> None:
-
         #Fügt leere Liste in für alle Produkte in die Historie ein
         for product in self.products:
             self.historical_market_trades[product] = []
-
-
+            self.daily_price[product] = []
 
     def run(self, state: TradingState) -> Dict[str, List[Order]]:
 
@@ -46,12 +49,12 @@ class Trader:
 
             if self.strategy[product] == 'all_time_avg':
                 orders: list[Order] = []
-
-                self.historical_market_trades[product].extend(state.market_trades[product])
+                if product in state.market_trades:
+                    self.historical_market_trades[product].extend(state.market_trades[product])
                 				
 				#Gets price from manually set price list in beginning
                 acceptable_price = self.avg_price[product]
-                '''
+
                 if len(self.historical_market_trades[product]) > 0:
                     #get prices and according position sizen from historical trades                  
                     prices = np.array([getattr(obj, 'price') for obj in self.historical_market_trades[product]])    
@@ -63,13 +66,12 @@ class Trader:
 
                 #get open orders   
                 order_depth: OrderDepth = state.order_depths[product]
-
                 #try to find sell order to buy from
                 if len(order_depth.sell_orders) > 0:
                     best_ask = min(order_depth.sell_orders.keys())
                     best_ask_volume = order_depth.sell_orders[best_ask]
                     if best_ask < acceptable_price:
-                        print(str(best_ask_volume) + ",", best_ask)
+                        #print(str(best_ask_volume) + ",", best_ask) #falls man loggen will
                         orders.append(Order(product, best_ask, -best_ask_volume))
 
                 #try to find buy order to sell to
@@ -77,56 +79,50 @@ class Trader:
                     best_bid = max(order_depth.buy_orders.keys())
                     best_bid_volume = order_depth.buy_orders[best_bid]
                     if best_bid > acceptable_price:
-                        print(str(best_bid_volume) + ",", best_bid)
+                        #print(str(best_bid_volume) + ",", best_bid) #falls man loggen will
                         orders.append(Order(product, best_bid, -best_bid_volume))
-`               '''
                 result[product] = orders
 
-            '''
-            if self.strategy[product] == 'floating_avg_fifty':
-                order_depth: OrderDepth = state.order_depths[product]
 
+            if self.strategy[product] == 'floating_avg':
                 orders: list[Order] = []				
-				
-                acceptable_price = self.avg_price[product]
-
-                banana_trades = pd.DataFrame(columns = ['quantity','price'])
+                if product in state.market_trades:
+                    self.historical_market_trades[product].extend(state.market_trades[product])
                 
-                for trade in state.market_trades[product]:
-                    banana_trades = pd.concat([banana_trades, pd.DataFrame([[trade.quantity, trade.price]], columns = ['quantity','price'])])	 		 
-                                   
-                if not banana_trades.empty:
-                    banana_price_avg = np.average(a = banana_trades['price'], weights = banana_trades['quantity'])
-                    index = self.iteration_count % self.number_of_trades
-                    self.banana_array_fifty[index] = banana_price_avg
-                    print(banana_price_avg)
+                acceptable_price = self.avg_price[product]	 		 
 
+                daily_price_avg = 0 #das ist noch sehr dummmmmmm
+                if len(state.market_trades[product]) > 0:
+                    prices = np.array([getattr(obj, 'price') for obj in state.market_trades[product]])    
+                    w = np.array([getattr(obj, 'price') for obj in state.market_trades[product]])
+                    daily_price_avg = np.average(a = prices, weights = w)    
+                    print(daily_price_avg)
+                
+                self.daily_price[product].append(daily_price_avg)
+                
+                floating_avg_start = max(self.iteration_count-50, 0)
 
-                acceptable_price = np.average(self.banana_array_fifty)
-
+                acceptable_price = np.average(self.daily_price[product][floating_avg_start:self.iteration_count])
                 print(acceptable_price)
                 
+                #get open orders   
                 order_depth: OrderDepth = state.order_depths[product]
-
+                #try to find sell order to buy from
                 if len(order_depth.sell_orders) > 0:
-
                     best_ask = min(order_depth.sell_orders.keys())
                     best_ask_volume = order_depth.sell_orders[best_ask]
-
                     if best_ask < acceptable_price:
-
-                        print(str(best_ask_volume) + ",", best_ask)
+                        #print(str(best_ask_volume) + ",", best_ask) #falls man loggen will
                         orders.append(Order(product, best_ask, -best_ask_volume))
 
+                #try to find buy order to sell to
                 if len(order_depth.buy_orders) != 0:
                     best_bid = max(order_depth.buy_orders.keys())
                     best_bid_volume = order_depth.buy_orders[best_bid]
                     if best_bid > acceptable_price:
-                        print(str(best_bid_volume) + ",", best_bid)
+                        #print(str(best_bid_volume) + ",", best_bid) #falls man loggen will
                         orders.append(Order(product, best_bid, -best_bid_volume))
-
                 result[product] = orders
-            '''
 
         self.iteration_count += 1
         print(str(self.iteration_count))
